fae1070d69795c8956436f84ab0d2000207f30a0
Victoria Kariolic <niaskywalk@gmail.com>
2020-11-09 17:48:24 -0500

    Feat: Connect Firebase; Enable persistance for create, read, delete

--- src/App.js
+++ src/App.js
@@ -5,84 +5,91 @@ import TaskCard from './TaskCard';
 import AppHeader from './AppHeader';
 import ListForm from './ListForm';
 import TaskForm from './TaskForm';
+// import {addList} from './api';
+import firebaseApp from './firebase';
+import { debounce } from 'lodash';
 
 
 class App extends React.Component {
   state = {
-    lists: [{
-      id: "1",
-      taskItems: [{
-        id: "1",
-        text: "Make ice cream",
-        complete: false
-      }, {
-        id: "2",
-        text: "Buy junk food",
-        complete: false
-      }, {
-        id: "3",
-        text: "Go to bed",
-        complete: true
-      }, {
-        id: "4",
-        text: "Jump on the bed",
-        complete: false
-      }],
-      title: "Do this Tomorrow",
-      description: "Describe this List"
-    },{
-      id: "2",
-      taskItems: [{
-        id: "1",
-        text: "Make ice cream",
-        complete: false
-      }, {
-        id: "2",
-        text: "Eat junk food",
-        complete: false
-      }, {
-        id: "3",
-        text: "Go to bed",
-        complete: true
-      }],
-      title: "Do this today",
-      description: "Describe this List"
-    },{
-      id: "3",
-      taskItems: [{
-        id: "1",
-        text: "Make ice cream",
-        complete: false
-      }, {
-        id: "2",
-        text: "Buy junk food",
-        complete: false
-      }, {
-        id: "3",
-        text: "Go to bed",
-        complete: true
-      }],
-      title: "Do this today",
-      description: "Describe this List"
-    },{
-      id: "4",
-      taskItems: [{
-        id: "1",
-        text: "Make ice cream",
-        complete: false
-      }, {
-        id: "2",
-        text: "Buy junk food",
-        complete: false
-      }, {
-        id: "3",
-        text: "Go to bed",
-        complete: true
-      }],
-      title: "Do this Never",
-      description: "Describe this List"
-    }]
+    lists: []
   }
+  // state = {
+  //   lists: [{
+  //     id: "1",
+  //     taskItems: [{
+  //       id: "1",
+  //       text: "Make ice cream",
+  //       complete: false
+  //     }, {
+  //       id: "2",
+  //       text: "Buy junk food",
+  //       complete: false
+  //     }, {
+  //       id: "3",
+  //       text: "Go to bed",
+  //       complete: true
+  //     }, {
+  //       id: "4",
+  //       text: "Jump on the bed",
+  //       complete: false
+  //     }],
+  //     title: "Do this Tomorrow",
+  //     description: "Describe this List"
+  //   },{
+  //     id: "2",
+  //     taskItems: [{
+  //       id: "1",
+  //       text: "Make ice cream",
+  //       complete: false
+  //     }, {
+  //       id: "2",
+  //       text: "Eat junk food",
+  //       complete: false
+  //     }, {
+  //       id: "3",
+  //       text: "Go to bed",
+  //       complete: true
+  //     }],
+  //     title: "Do this today",
+  //     description: "Describe this List"
+  //   },{
+  //     id: "3",
+  //     taskItems: [{
+  //       id: "1",
+  //       text: "Make ice cream",
+  //       complete: false
+  //     }, {
+  //       id: "2",
+  //       text: "Buy junk food",
+  //       complete: false
+  //     }, {
+  //       id: "3",
+  //       text: "Go to bed",
+  //       complete: true
+  //     }],
+  //     title: "Do this today",
+  //     description: "Describe this List"
+  //   },{
+  //     id: "4",
+  //     taskItems: [{
+  //       id: "1",
+  //       text: "Make ice cream",
+  //       complete: false
+  //     }, {
+  //       id: "2",
+  //       text: "Buy junk food",
+  //       complete: false
+  //     }, {
+  //       id: "3",
+  //       text: "Go to bed",
+  //       complete: true
+  //     }],
+  //     title: "Do this Never",
+  //     description: "Describe this List"
+  //   }]
+  // }
+  
   
   toggleTaskItem = (listID, taskID) => {
     const {lists} = this.state;
@@ -118,7 +125,9 @@ class App extends React.Component {
       ]
     }
     this.setState(newState);
+    firebaseApp.database().ref(`lists/${listID}/taskItems/${taskID}/complete`).set(!foundTask.complete);
   }
+
   removeTaskItem = (listID, taskID) => {
     const {lists} = this.state;
     const listIndex = lists.findIndex(list => list.id === listID);
@@ -129,7 +138,7 @@ class App extends React.Component {
     const taskIndex = foundList.taskItems.findIndex(task => task.id === taskID);
     if (taskIndex < 0) {
       throw new Error("Task ID Not Found");
-    }import {addList} from './api';
+    }
 
     const modifiedList = {
       ...foundList,
@@ -148,6 +157,7 @@ class App extends React.Component {
       ]
     }
     this.setState(newState);
+    firebaseApp.database().ref(`lists/${listID}/taskItems/${taskID}`).remove();
   } 
 
   addNewList = (listName, listDesc, initialItem) => {
@@ -171,8 +181,15 @@ class App extends React.Component {
       lists: [...lists, newList]
     };
 
-    this.setState(newState);
-
+    this.setState(newState,() => {
+      const ref = firebaseApp.database().ref(`lists`).push({...newList, id: null});
+      this.setState({
+        lists: [
+          ...this.state.lists.slice(0,-1), {...this.state.lists[this.state.lists.length - 1], id: ref.key}
+        ]
+      })
+    });
+    
   }
 
   removeList = (listID) => {
@@ -190,6 +207,7 @@ class App extends React.Component {
     }
 
     this.setState(newState);
+    firebaseApp.database().ref(`lists/${listID}`).remove();
   }
 
   addNewTask = (listID, taskText) => {
@@ -221,30 +239,76 @@ class App extends React.Component {
           ...lists.slice(listIndex+1)
         ]
       }
-      this.setState(newState);
+      this.setState(newState, () => { 
+        const { lists } = this.state;
+        const ref = firebaseApp.database().ref(`lists/${listID}/taskItems`).push({text: taskText, complete: false});
+        this.setState({
+          lists: [
+            ...lists.slice(0,listIndex),
+            {
+              ...lists[listIndex],
+              taskItems: [
+                ...lists[listIndex].taskItems.slice(0,-1),
+                {
+                  ...lists[listIndex].taskItems[lists[listIndex].taskItems.length - 1],
+                   id: ref.key
+                }
+              ]
+            },
+            ...lists.slice(listIndex+1)
+          ]
+        });
+      });
+      
+
+
     }
 
   }
+  
+  convertFirebaseData = (lists) => {
+       
+    const tempList = [];
+    for (const key in lists) {
+      const newList = { ...lists[key], id: key, taskItems: [] };
+      for (const taskItemKey in lists[key].taskItems) {
+        newList.taskItems.push({ ...lists[key].taskItems[taskItemKey], id: taskItemKey });
+      }
+      tempList.push(newList);
+    }
+    return tempList;
+  };
+
+  componentDidMount = () => {
+    firebaseApp.database().ref("lists").once("value").then(snapshot => {
+      this.setState({
+        lists: this.convertFirebaseData(snapshot.val())
+      });
+    });
+  }
 
   render () {
-    const {lists} = this.state;
+    const { lists = [] } = this.state;
     return (
+      
       <div className="appWrapper">
         <AppHeader />
         <main className="appTaskWrapper">
           {this.state.lists.map(list => 
             <TaskCard 
               key={list.id}
-              toggleTaskItem={this.toggleTaskItem} 
+              toggleTaskItem={debounce(this.toggleTaskItem, 100)} 
               listData={list}
               removeTaskItem={this.removeTaskItem}
               onDelete={this.removeList}
             />
+
           )}
         </main>
         <ListForm onNewList={this.addNewList} />
         <TaskForm onNewTask={this.addNewTask} lists={lists} />
       </div>
+      
     );
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyB6NaiaYKfOfMHfMxi_5nBjvddjRzjXiMg",
+  authDomain: "todolistapp-murasaki.firebaseapp.com",
+  databaseURL: "https://todolistapp-murasaki.firebaseio.com",
+  projectId: "todolistapp-murasaki",
+  storageBucket: "todolistapp-murasaki.appspot.com",
+  messagingSenderId: "455803529738",
+  appId: "1:455803529738:web:cfc56a177f6d104d1b8df3",
+  measurementId: "G-1DG22PH75G"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+// window.firebase.analytics();
+
+export default firebaseApp;
